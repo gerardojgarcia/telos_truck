11ca31a57d57eda02624052794aed32568e66d9c
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-06-27 07:27:15 -0700

    Edited AppHero Layout

--- components/AppHero.vue
+++ components/AppHero.vue
@@ -1,25 +1,11 @@
-<script setup>
+<script setup lang="ts"></script>
 
-</script>
-
-<template >
-<div id="hero">
-
-
-<div class="hero__text">
-
-
-<h1 class=" hero__text-title text-6xl">Hello</h1>
-
-</div>
-
-</div>
-
+<template>
+  <div id="hero">
+    <div class="hero__text">
+      <h1 class="hero__text-title text-6xl">Telos is the future of utility</h1>
+    </div>
+  </div>
 </template>
 
-<style lang="css" scoped>
-
-
-
-
-</style>
+<style lang="css" scoped></style>
