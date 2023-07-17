f3b2c4f91d594689fdd19f6977c7a64c6c82db61
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-07-17 06:05:32 -0700

    updated features section

--- components/AppFeatures.vue
+++ components/AppFeatures.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div id="features" class="py-16">
-    <div class="px-52">
-      <h2 class="features-title text-5xl text-gray-400">
+    <div class="px-52 flex flex-col">
+      <h2 class="features-title text-5xl text-gray-400 w-">
         TELOS trucks makes the world's most efficient EV pickup for urban living and
         weekend adventuring
       </h2>
 
-      <div class="features__list flex pt-16 w-3/4">
-        <div class="list-line border-b-2 w-1/2 self-start"></div>
+      <div class="features__list flex py-16 w-3/4 bg-transparent">
+        <div class="list-line border-b-2 w-1/2 self-start mx-16 pt-4"></div>
         <div class="list-text space-y-8 w-1/2">
           <p>
             We redesigned the EVtruck footprint wand function fomr the ground up by
@@ -21,12 +21,28 @@
             footprint of a MINI Cooper, TELOS is the most compact, practical and
             technically advanced truck.
           </p>
-          <p>
+          <p class="pb-8">
             Meeting the need for a highly functional and powerful EV pickup equally suited
             to navigating downtown adn hauling people adn gear out of town.
           </p>
-          <NuxtLink class="features-link uppercase font-bold">Reserve Yours</NuxtLink>
+          <NuxtLink class="features-link uppercase font-bold cursor-pointer"
+            >Reserve Yours</NuxtLink
+          >
         </div>
+      </div>
+      <div
+        class="features__images flex flex-col md:flex-row items-center justify-center pt-24 w-3/4 self-center"
+      >
+        <nuxt-img
+          src="/truck2.webp"
+          sizes="sm:100vw md:50vw lg:35vw"
+          id="features-img-1"
+        ></nuxt-img>
+        <nuxt-img
+          src="/truck9.webp"
+          sizes="sm:100vw md:50vw lg:35vw"
+          id="features-img-2"
+        ></nuxt-img>
       </div>
     </div>
   </div>
@@ -34,5 +50,24 @@
 <style scoped>
 * {
   color: black;
+}
+
+.features-link {
+  border-bottom: 0.2em solid #56bc5a;
+
+  padding-bottom: 0.3rem;
+}
+
+.list-line {
+  background-color: transparent;
+}
+
+#features-img-1 {
+  transform: translateY(-5rem);
+  z-index: 2;
+}
+
+#features-img-2 {
+  transform: translateY(2rem) translateX(-5rem);
 }
 </style>
