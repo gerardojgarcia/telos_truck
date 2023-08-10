a5a2281ff500119a86a055e69b8f8ebd8f9f23be
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-08-09 18:18:54 -0700

    configuration section updates

--- components/AppConfiguration.vue
+++ components/AppConfiguration.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template >
-  <div id="configuration" class="configuration">
-    <div class="configuration-title flex flex-col md:flex-row ">
-<div class="">
-  <h5 class="text-black">Built to handle everyting you need</h5>
+  <div id="configuration" class="configuration flex flex-col py-32ç">
+    <div class="configuration-title flex flex-col md:flex-row justify-center ">
+<div class="configuration-title-text ml-52 w-1/3">
+  <h5 class="text-black text-5xl">Built to handle everyting you need</h5>
   <div
     class="text-btns py-6 flex justify-center items-center self-end bg-transparent"
   >
@@ -24,12 +24,20 @@
 
       <nuxt-img
         src="/truck6.webp"
-        sizes="sm:100vw md:50vw lg:40vw"
+        sizes="sm:100vw md:50vw lg:65vw"
         id="features-img-1"
-        class="grayscale hover:filter-none duration-150"
+        class="grayscale hover:filter-none duration-150 2/3"
       ></nuxt-img>
     </div>
 
+<div class="configuration-description text-gray-400 w-1/2 self-center pl-64">
+
+  <p class="text-black">TELOS offers a 60 degree truck bed that, when extended into bacskeat thanks to an innovative midgate, an accomamodate 4' x 8' plywood sheets or a 8' surboard while the tailgate is up. </p>
+  <p class="text-black">TELOS' specially designed storage tunnel under the bed doubles asa footwel lfor a thrid row of seats, enabling TELOS to accommodate up to eigh pasengers. Belongings are safely secured by the tonneau cover, makeing TELOS teh perfcet vehicle to safely store work items and weekend gear while on the go.</p>
+
+
+
+</div>
 
 
 
