92fbb91d60ac29658e4172a4c01ebe18c13e20f5
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-08-11 10:41:05 -0700

    added new image assets, completed flexible section layout

--- components/AppFlexible.vue
+++ components/AppFlexible.vue
@@ -9,10 +9,10 @@
 
 <div id="flexibility" class="flexibilty grid grid-cols-1 py-32 space-y-24">
   <div class="flex space-x-32 px-52">
-<nuxt-img src="/truck6.webp"></nuxt-img>
+<nuxt-img src="/telos14.png" sizes="lg:35vw"></nuxt-img>
 <div class="flex flex-col">
 <strong class="text-4xl text-black mb-24">Room to Roam</strong>
-<p class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p class="text-black">Offering a 350-mile range, 105kWh, 20-minute (20-80%) fast charging battery pack that fits in the footrpint of a sub-subcompact vehicle. </p>
 </div>
 
 
@@ -21,19 +21,19 @@
   </div>
   <div class="flex space-x-32 px-52">
 
-    <div class="flex">
-
-    <p class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="flex flex-col">
+<strong class="text-4xl text-black mb-24">Seeing around corners</strong>
+    <p class="text-black">Utilizing the latest in advanced saftey technology - sensors to predict and classify collisions before they happen, airbags and structurual technology - to make our vehicles safer not only for occupants but also for pedestrians, bicyclists, and other vulnerable road users.</p>
     </div>
 
-    <nuxt-img src="/truck8.webp"></nuxt-img>
+    <nuxt-img src="/telos12.png" sizes="lg:55vw"></nuxt-img>
 
   </div>
   <div class="flex space-x-32 px-52">
-    <nuxt-img src="/truck4.webp" sizes="lg:50vw"></nuxt-img>
-    <div class="flex">
-
-    <p class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <nuxt-img src="/telos13.png" sizes="lg:40vw"></nuxt-img>
+    <div class="flex flex-col">
+<strong class="text-4xl text-black mb-24">Effortless agility</strong>
+    <p class="text-black">Easily manuever through tight traffic and trails with improved visibility and greater agiliity both on off-road.</p>
     </div>
 
   </div>
