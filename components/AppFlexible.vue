c2dbc388fe06fffb246de5989227eedb5bb1a36b
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-08-11 10:57:34 -0700

    flexible section update

--- components/AppFlexible.vue
+++ components/AppFlexible.vue
@@ -10,9 +10,9 @@
 <div id="flexibility" class="flexibilty grid grid-cols-1 py-32 space-y-24">
   <div class="flex space-x-32 px-52">
 <nuxt-img src="/telos14.png" sizes="lg:35vw"></nuxt-img>
-<div class="flex flex-col">
-<strong class="text-4xl text-black mb-24">Room to Roam</strong>
-<p class="text-black">Offering a 350-mile range, 105kWh, 20-minute (20-80%) fast charging battery pack that fits in the footrpint of a sub-subcompact vehicle. </p>
+<div class="flex flex-col mt-16">
+<strong class="text-4xl text-black mb-12 font-thin">Room to Roam</strong>
+<p class="text-gray-400 w-1/2">Offering a 350-mile range, 105kWh, 20-minute (20-80%) fast charging battery pack that fits in the footrpint of a sub-subcompact vehicle. </p>
 </div>
 
 
@@ -21,9 +21,9 @@
   </div>
   <div class="flex space-x-32 px-52">
 
-    <div class="flex flex-col">
-<strong class="text-4xl text-black mb-24">Seeing around corners</strong>
-    <p class="text-black">Utilizing the latest in advanced saftey technology - sensors to predict and classify collisions before they happen, airbags and structurual technology - to make our vehicles safer not only for occupants but also for pedestrians, bicyclists, and other vulnerable road users.</p>
+    <div class="flex flex-col mt-32">
+<strong class="text-4xl text-black mb-12 font-thin">Seeing around corners</strong>
+    <p class="text-gray-400 ">Utilizing the latest in advanced saftey technology - sensors to predict and classify collisions before they happen, airbags and structurual technology - to make our vehicles safer not only for occupants but also for pedestrians, bicyclists, and other vulnerable road users.</p>
     </div>
 
     <nuxt-img src="/telos12.png" sizes="lg:55vw"></nuxt-img>
@@ -31,9 +31,9 @@
   </div>
   <div class="flex space-x-32 px-52">
     <nuxt-img src="/telos13.png" sizes="lg:40vw"></nuxt-img>
-    <div class="flex flex-col">
-<strong class="text-4xl text-black mb-24">Effortless agility</strong>
-    <p class="text-black">Easily manuever through tight traffic and trails with improved visibility and greater agiliity both on off-road.</p>
+    <div class="flex flex-col mt-32">
+<strong class="text-4xl text-black mb-12 font-thin">Effortless agility</strong>
+    <p class="text-gray-400 w-1/2">Easily manuever through tight traffic and trails with improved visibility and greater agiliity both on off-road.</p>
     </div>
 
   </div>
