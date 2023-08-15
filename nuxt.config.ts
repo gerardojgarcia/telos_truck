5a2118758e89bef6e5f677e7359b620493412444
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-08-14 23:30:23 -0700

    added compression to static production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,31 @@ export default defineNuxtConfig({
 
   ],
 
+
+  app: {
+		head: {
+
+			charset: 'utf-16',
+			viewport: 'width=device-width, initial-scale=1',
+			title: 'TELOS truck - the future of adventure transport',
+			meta: [
+			// <meta name="description" content="My amazing site">
+			{ name: 'description', content: 'TELOS - the future of compact trucks' }
+			 ],
+			 htmlAttrs: {
+				lang: 'en',
+			  },
+		}
+	},
+
   telemetry: false,
+
+  nitro: {
+		compressPublicAssets: true,
+		prerender: {
+			crawlLinks: true,
+		}
+	},
 
 
 
