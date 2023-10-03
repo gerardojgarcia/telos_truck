7acace1916e28a64810b88d3c60232e30fab79f3
Gerardo Garcia <gerardojgarcia@pm.me>
2023-10-02 17:53:14 -0700

    updated business logos

--- components/AppPartners.vue
+++ components/AppPartners.vue
@@ -7,22 +7,22 @@ const partners = ref([
   },
   {
     id: 2,
-    logo: "/wall_street.webp",
-    name: "telos",
+    logo: "/The_Wall_Street_Journal-Logo.wine.png",
+    name: "Wall Street Journal",
   },
+  {
+    id: 2,
+    logo: "/pngwing.com.png",
+    name: "TechCrunch",
+
+  }
 ]);
 </script>
 <template>
   <div class="flex space-x-4">
-    <div
-      class="partner flex items-center"
-      v-for="partner in partners"
-      :key="partner.id"
-    >
-      <nuxt-img :src="partner.logo" sizes="sm:90vw lg:25vw"></nuxt-img
-      ><strong class="partner-name text-black text-3xl">{{
-        partner.name
-      }}</strong>
+    <div class="partner flex items-center" v-for="partner in partners" :key="partner.id">
+      <nuxt-img :src="partner.logo" sizes="sm:90vw lg:25vw" format="webp" :alt="partner.name"></nuxt-img>
+
     </div>
   </div>
 </template>
